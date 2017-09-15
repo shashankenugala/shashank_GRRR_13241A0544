11ac81364285fe688c8ba2096bb7aa50f27a4722
shashankenugala <31987609+shashankenugala@users.noreply.github.com>
2017-09-15 22:16:43 +0530

    third

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import java.text.SimpleDateFormat;
-import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
@@ -7,14 +5,6 @@ public class Main {
 		//You may test that your code works find here
 		//Please check that your code works and has no 
 		//compilation problems before to submit
-		try{
-		//StudentGroup sg=new StudentGroup(5);
-		String date1="1995-07-21";
-		Date d1=new SimpleDateFormat("yyyy-mm-dd").parse(date1);
-		Student s1=new Student(1,"rakesh adepu",d1,70.55);
-		System.out.println(s1.getId());
-		}
-		catch(Exception e){e.printStackTrace();}
 	}
 
 }
